4938fd4dde18a4d2b00583d745caf52564e7ca85
howen02 <wenhyap02@gmail.com>
2024-06-25 23:04:25 +0800

    Add rate limiting middleware with requests

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"database/sql"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
+
+const REQUESTS_PER_SECOND = 10
 
 func main() {
 	db := InitialiseDB()
@@ -13,10 +16,32 @@ func main() {
 
 	r := gin.Default()
 
+	r.Use(rateLimiter(REQUESTS_PER_SECOND))
 	r.GET("/shorten", handleShorten(db))
 	r.GET("/lengthen", handleLengthen(db))
 
 	r.Run(":3000")
+}
+
+func rateLimiter(frequency int) gin.HandlerFunc {
+	ticker := time.NewTicker(time.Second / time.Duration(frequency))
+	channel := make(chan time.Time, 1)
+
+	go func() {
+		for t:= range ticker.C {
+			channel <- t
+		}
+	}()
+
+	return func(c *gin.Context) {
+		select {
+		case <-channel:
+			c.Next()
+		default:
+			c.JSON(http.StatusTooManyRequests, gin.H{"error": "Rate limit exceeded, try again"})
+			c.Abort()
+		}
+	}
 }
 
 func handleShorten(db *sql.DB) gin.HandlerFunc {
--- tests/api_test.go
+++ tests/api_test.go
@@ -12,7 +12,7 @@ func TestShortenEndpoint(t *testing.T) {
 	fmt.Println("Testing shorten endpoint")
 	resp, err := http.Get("http://localhost:3000/shorten?longUrl=http://www.google.com")
 
-	if err != nil || resp.StatusCode != http.StatusOK {
+	if err != nil || resp.StatusCode != http.StatusOK{
 		log.Fatal("Error getting shortUrl: ", err)
 	}	
 	defer resp.Body.Close()
@@ -42,5 +42,37 @@ func TestLengthenEndopint(t *testing.T) {
 	}
 
 	fmt.Println(string(body))
+	fmt.Println("")
+}
+
+func TestRateLimiterExceed(t *testing.T) {
+	requests := 20
+	fmt.Println("Testing rate limiter with exceeding requests")
+
+	for i := 0; i < requests; i++ {
+		resp, err := http.Get("http://localhost:3000/shorten?longUrl=http://www.google.com")
+
+		if err != nil && resp.StatusCode != http.StatusTooManyRequests {
+			log.Fatal("Rate limiter test failed")
+		}
+	}
+
+	fmt.Printf("Rate limiter test passed with %d requests\n", requests)
+	fmt.Println("")	
+}
+
+func TestRateLimiterWithin(t *testing.T) {
+	requests := 10
+	fmt.Println("Testing rate limiter within requests")
+
+	for i := 0; i < 10; i++ {
+		resp, err := http.Get("http://localhost:3000/shorten?longUrl=http://www.google.com")
+
+		if err != nil && resp.StatusCode != http.StatusOK {
+			log.Fatal("Rate limiter test failed")
+		}
+	}
+
+	fmt.Printf("Rate limiter test passed with %d requests\n", requests)
 	fmt.Println("")
 }
